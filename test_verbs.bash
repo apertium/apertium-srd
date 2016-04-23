ac295364f443eea6efcc66e4b9858fb88679ce88
Hèctor Alòs i Font <hectoralos@gmail.com>
2016-04-23 20:01:40 +0000

    paradigma 'tènnere'

---
+++ test_verbs.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+#verb='èssere'
+verb='tènnere'
+
+s="^$verb<vblex><inf>$"
+echo "$s" | lt-proc -g srd.autogen.bin
+s="^$verb<vblex><ger>$"
+echo "$s" | lt-proc -g srd.autogen.bin
+s="^$verb<vblex><pp><m><sg>$"
+echo "$s" | lt-proc -g srd.autogen.bin
+s="^$verb<vblex><pp><f><sg>$"
+echo "$s" | lt-proc -g srd.autogen.bin
+s="^$verb<vblex><pp><m><pl>$"
+echo "$s" | lt-proc -g srd.autogen.bin
+s="^$verb<vblex><pp><f><pl>$"
+echo "$s" | lt-proc -g srd.autogen.bin
+echo ""
+
+for temps in pri pii prs pis imp
+do
+	for sg in sg pl
+	do
+		for pers in p1 p2 p3
+		do
+			s="^$verb<vblex><$temps><$pers><$sg>\$"
+#			echo $s
+			echo "$s" | lt-proc -g srd.autogen.bin
+		done
+	done
+echo ""
+done
